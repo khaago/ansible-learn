10e07b6ecd1fd3f1bd61b9780ecf237e2d1d86d8
khaago <aashishbhattarai@gmail.com>
2020-07-22 20:44:46 -0400

    tillvars

--- cft.sh
+++ cft.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env zsh
+
 if [ $1 == 'create' ]; then
-	aws cloudformation create-stack --stack-name ansible-course --template-body file://setup-env.yml 
+	aws cloudformation create-stack --stack-name ansible-course \
+	--template-body file://setup-env.yml \
+	--parameters ParameterKey=KeyName,ParameterValue=ansible-course-key-pair ParameterKey=NameOfService,ParameterValue=ansible-course
+
 fi
 
 if [ $1 == 'delete' ]; then
